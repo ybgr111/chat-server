d004c41b5d8474c250230add8f9b40deebe72a2c
ybgr111 <ybgr111@mail.ru>
2024-02-10 18:22:43 +0300

    Первое домашнее задание

--- Makefile
+++ Makefile
@@ -1,7 +1,30 @@
 LOCAL_BIN:=$(CURDIR)/bin
 
+##linter
 install-golangci-lint:
 	GOBIN=$(LOCAL_BIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.53.3
 
 lint:
 	GOBIN=$(LOCAL_BIN) golangci-lint run ./... --config .golangci.pipeline.yaml
+
+##proto; grpc
+install-deps:
+	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28.1
+	GOBIN=$(LOCAL_BIN) go install -mod=mod google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+get-deps:
+	go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+	go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+
+generate:
+	make generate-note-api
+
+generate-note-api:
+	mkdir -p pkg/note_v1
+	protoc --proto_path grpc/api/note_v1 \
+	--go_out=grpc/pkg/note_v1 --go_opt=paths=source_relative \
+	--plugin=protoc-gen-go=bin/protoc-gen-go \
+	--go-grpc_out=grpc/pkg/note_v1 --go-grpc_opt=paths=source_relative \
+	--plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
+	grpc/api/note_v1/note.proto
---
+++ grpc/cmd/grpc_client/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/brianvoe/gofakeit"
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/ybgr111/chat-server/grpc/pkg/note_v1"
+)
+
+const (
+	address = "localhost:50053"
+	chatID  = 12
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewNoteV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	create, err := c.Create(ctx, &desc.CreateRequest{
+		Users: &desc.Users{
+			Usernames: []string{"Nikita", "Eshe Nikita"},
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to create chat: %v", err)
+	}
+	log.Printf(color.RedString("Сreate chat info:\n"), color.GreenString("%+v", create.GetId()))
+
+	delete, err := c.Delete(ctx, &desc.DeleteRequest{
+		Id: chatID,
+	})
+	if err != nil {
+		log.Fatalf("failed to delete chat by id: %v", err)
+	}
+	log.Printf(color.RedString("Delete chat info:\n"), color.GreenString("%+v", delete.String()))
+
+	sendMessage, err := c.SendMessage(ctx, &desc.SendMessageRequest{
+		Message: &desc.Message{
+			From:      gofakeit.Name(),
+			Text:      gofakeit.BeerHop(),
+			Timestamp: timestamppb.New(gofakeit.Date()),
+		},
+	})
+	if err != nil {
+		log.Fatalf("failed to send massage: %v", err)
+	}
+	log.Printf(color.RedString("Send message info:\n"), color.GreenString("%+v", sendMessage.String()))
+}
